5bc271fa066110f4e4f112804ab2d0059ef81ee8
silvertxd <silvertxd@gmail.com>
2024-02-22 22:50:48 +0200

    removed header border

--- frontend/src/components/ui/Header.tsx
+++ frontend/src/components/ui/Header.tsx
@@ -1,12 +1,12 @@
 import NavLink from './NavLink';
 const Header = () => {
 	return (
-		<header className='py-5 border-b border-gray-200'>
+		<header className='py-5'>
 			<div className='container mx-auto flex items-center justify-between'>
-				<div className='flex items-center'>
+				<div className='flex items-center '>
 					<h1 className='text-xl font-bold tracking-tight'>DotaDataDive</h1>
 				</div>
-				<div className='flex items-center justify-center space-x-6'>
+				<div className='flex items-center justify-center space-x-6 w-full'>
 					<NavLink to='/' label='Home' />
 					<NavLink to='/meta' label='Meta' />
 					<NavLink to='/heroes' label='Heroes' />
