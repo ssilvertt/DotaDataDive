f153ea4f5001b181efab15fee08ccb0113a34ad0
silvertxd <silvertxd@gmail.com>
2024-02-22 22:50:21 +0200

    added meta table background + shadow

--- frontend/src/components/views/Meta.tsx
+++ frontend/src/components/views/Meta.tsx
@@ -1,10 +1,13 @@
 import TabsComponent from '../ui/HeroMeta/TabsComponent/TabsComponent';
-import styled from 'styled-components';
+import {styled, createGlobalStyle} from 'styled-components';
+import bg from '@/assets/generic_hero_background_png.png';
+
 
 const BackgroundImageLink = styled.div`
   background-image: url('https://cdn.cloudflare.steamstatic.com/apps/dota2/images/outlanders/patch-notes/bg_repeater.jpg');
-
 `;
+
+
 
 const Meta = () => {
 	return (
