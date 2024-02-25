5be4cb75f1ea4116274993787038dc0a36116dbc
silvertxd <silvertxd@gmail.com>
2024-02-25 23:44:56 +0200

    fixed spacing

--- frontend/src/components/ui/Heroes/HeroRow/HeroRow.tsx
+++ frontend/src/components/ui/Heroes/HeroRow/HeroRow.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
 
 interface HeroRowProps {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }
 
-const   HeroRow = ({ children }: HeroRowProps) => {
-  return (
-    <div className="flex flex-row flex-grow-1 basis-0">
-      {children}
-    </div>
-  );
+const HeroRow = ({ children }: HeroRowProps) => {
+	return <div className='flex flex-row flex-grow-1 basis-0'>{children}</div>;
 };
-
 
 export default HeroRow;
