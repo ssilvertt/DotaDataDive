22f631609b9bc1b7cbb6bd716af90b566e5a9a38
silvertxd <silvertxd@gmail.com>
2024-02-25 23:24:50 +0200

    removed styled background

--- frontend/src/components/views/Meta.tsx
+++ frontend/src/components/views/Meta.tsx
@@ -1,20 +1,7 @@
 import TabsComponent from '../ui/HeroMeta/TabsComponent/TabsComponent';
-import {styled, createGlobalStyle} from 'styled-components';
-import bg from '@/assets/generic_hero_background_png.png';
-
-
-const BackgroundImageLink = styled.div`
-  background-image: url('https://cdn.cloudflare.steamstatic.com/apps/dota2/images/outlanders/patch-notes/bg_repeater.jpg');
-`;
-
-
 
 const Meta = () => {
-	return (
-		<BackgroundImageLink>
-			<TabsComponent />
-		</BackgroundImageLink>
-	);
+	return <TabsComponent />;
 };
 
 export default Meta;
