9a2cb39cd6eab8a80e78bd0ce25f8a5f65b86331
silvertxd <silvertxd@gmail.com>
2024-02-27 16:56:53 +0200

    removed unused function

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -4,17 +4,3 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-
-const cacheImages =async (images:string[]) => {
-  const promises = await images.map((src) =>{
-    return new Promise(function(resolve, reject){
-      const img = new Image();
-
-      img.src = src;
-      img.onload = () => resolve();
-      img.onerror = reject;
-    })
-  })
-
-  await Promise.all(promises);
-}
