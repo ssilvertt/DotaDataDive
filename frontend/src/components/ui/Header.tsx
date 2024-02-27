360618beafba23ae343506266adf5925f8627b66
silvertxd <silvertxd@gmail.com>
2024-02-27 18:38:41 +0200

    fixed header for mobile

--- frontend/src/components/ui/Header.tsx
+++ frontend/src/components/ui/Header.tsx
@@ -1,25 +1,22 @@
-import NavButton from './NavButton';
 import { NavLink } from 'react-router-dom';
+import NavButton from './NavButton';
 const Header = () => {
 	return (
-		<header className=''>
-			<div className=' text-white px-6 py-4 flex items-center'>
-				<div className='flex space-x-4 items-center text-[#d6dadd] hover:text-white'>
-					<div className='pl-10'>
+		<header>
+			<div className=' text-white px-6 py-4 flex items-center justify-center lg:justify-normal'>
+				<div className='flex space-x-4 items-center text-[#d6dadd] hover:text-white order-2 lg:order-1'>
+					<div className='pl-10 pr-10'>
 						<NavLink to='/'>
 							<h3 className='scroll-m-20 text-2xl font-semibold tracking-tight'>
 								DotaDataDive
 							</h3>
 						</NavLink>
 					</div>
-					<div className='space-x-2'>
-						<i className='fas fa-icon1'></i>
-						<i className='fas fa-icon2'></i>
-						<i className='fas fa-icon3'></i>
-					</div>
 				</div>
-				<div className='space-x-4 '>
+				<div className='order-1 lg:order-2 lg:pr-4'>
 					<NavButton to='/'>META</NavButton>
+				</div>
+				<div className='order-3 lg:order-3'>
 					<NavButton to='/heroes'>HEROES</NavButton>
 				</div>
 			</div>
