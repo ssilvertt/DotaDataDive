b1bfbb31a0d3f22b5d0b286a940f43b209657f62
silvertxd <silvertxd@gmail.com>
2024-02-28 23:28:24 +0200

    Update herobackgrounds.ts

--- frontend/src/utils/herobackgrounds.ts
+++ frontend/src/utils/herobackgrounds.ts
@@ -1,3 +1,4 @@
+import img from '../assets/hero-backgrounds/loadout_bg_rocky_003_jpg.jpeg'
 type HeroLoadoutBackground = {
   [key in
     | 'abaddon'
@@ -207,7 +208,7 @@ const heroLoadoutBackground: HeroLoadoutBackground = {
   razor: ["radial-gradient(circle at 50% 0%, rgba(27, 32, 49, 0.8), rgba(17, 16, 23))", '/src/assets/hero-backgrounds/loadout_bg_sky_002_jpg.jpeg'],
   riki: ["radial-gradient(circle at 50% 0%, rgba(27, 32, 49, 0.8), rgba(17, 16, 23))", '/src/assets/hero-backgrounds/loadout_bg_outworld_004_jpg.jpeg'],
   rubick: ["radial-gradient(circle at 50% 0%, rgba(24, 45, 40, 0.8), rgba(17, 16, 23))", '/src/assets/hero-backgrounds/loadout_bg_outworld_003_jpg.jpeg'],
-  sand_king: ["radial-gradient(circle at 50% 0%, rgba(44, 37, 31, 0.8), rgba(23, 16, 16))", '/src/assets/hero-backgrounds/loadout_bg_rocky_003_jpg.jpeg'],
+  sand_king: ["radial-gradient(circle at 50% 0%, rgba(44, 37, 31, 0.8), rgba(23, 16, 16))", img],
   shadow_demon: ["radial-gradient(circle at 50% 0%, rgba(44, 26, 31, 0.8), rgba(23, 16, 16))", '/src/assets/hero-backgrounds/loadout_bg_fire_002_jpg.jpeg'],
   nevermore: ["radial-gradient(circle at 50% 0%, rgba(39, 9, 9, 0.8), rgba(9, 5, 5))", '/src/assets/hero-backgrounds/loadout_bg_fire_004_jpg.jpeg'],
   shadow_shaman: ["radial-gradient(circle at 50% 0%, rgba(39, 29, 9, 0.8), rgba(14, 9, 9))", '/src/assets/hero-backgrounds/loadout_bg_rocky_002_jpg.jpeg'],
