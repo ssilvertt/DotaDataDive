b94ea0e8c93693f7d8360152ce30531b12e8c6b4
silvertxd <silvertxd@gmail.com>
2024-02-28 23:05:59 +0200

    updating bg

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,28 +1,14 @@
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import { defineConfig } from 'vite';
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src'),
-		},
-	},
-	build: {
-		rollupOptions: {
-			output: {
-				manualChunks(id) {
-					if (id.includes('node_modules')) {
-						return id
-							.toString()
-							.split('node_modules/')[1]
-							.split('/')[0]
-							.toString();
-					}
-				},
-			},
-		},
-	},
-});
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  
+})
