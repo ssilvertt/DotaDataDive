376cc81c927c0bfafab8e16b0a8870d4cf0d2d98
silvertxd <silvertxd@gmail.com>
2024-02-28 14:06:33 +0200

    deleted unused imports

--- frontend/src/utils/apollo.ts
+++ frontend/src/utils/apollo.ts
@@ -1,6 +1,5 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client'
 import { setContext } from '@apollo/client/link/context'
-import { TOKEN } from '../config'
 
 const httpLink = createHttpLink({
 	uri: 'https://api.stratz.com/graphql',
