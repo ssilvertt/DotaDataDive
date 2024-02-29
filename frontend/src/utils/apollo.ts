594b37013b41d95c1bb7f0e446eb1492894b3934
silvertxd <silvertxd@gmail.com>
2024-02-29 12:58:07 +0200

    updated spaces

--- frontend/src/utils/apollo.ts
+++ frontend/src/utils/apollo.ts
@@ -1,40 +1,45 @@
-import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client'
-import { setContext } from '@apollo/client/link/context'
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
 	uri: 'https://api.stratz.com/graphql',
-})
+});
 
 const authLink = setContext((_, { headers }) => {
 	return {
 		headers: {
 			...headers,
-			authorization: import.meta.env.VITE_TOKEN ? `Bearer ${import.meta.env.VITE_TOKEN}` : '',
+			authorization: import.meta.env.VITE_TOKEN
+				? `Bearer ${import.meta.env.VITE_TOKEN}`
+				: '',
 		},
-	}
-})
+	};
+});
 
 const cache = new InMemoryCache({
-  typePolicies: {
-    Query: {
-      fields: {
-        heroStats: {
-          merge(existing, incoming) {
-            return {
-              ...existing,
-              ...incoming,
-              winWeek: [...(existing?.winWeek || []), ...(incoming?.winWeek || [])],
-            };
-          },
-        },
-      },
-    },
-  },
+	typePolicies: {
+		Query: {
+			fields: {
+				heroStats: {
+					merge(existing, incoming) {
+						return {
+							...existing,
+							...incoming,
+							winWeek: [
+								...(existing?.winWeek || []),
+								...(incoming?.winWeek || []),
+							],
+						};
+					},
+				},
+			},
+		},
+	},
 });
 
 const client = new ApolloClient({
 	link: authLink.concat(httpLink),
 	cache,
-})
+});
 
-export default client
+export default client;
